b128a95cb46362927bf9f30fd01ae0372abeee8d
octodaniil <dan.khegai@gmail.com>
2024-08-14 23:12:28 +0300

    Ready project (test)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,21 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [siteSetting, setSiteSettings] = useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': siteSetting.fontFamilyOption.value,
+					'--font-size': siteSetting.fontSizeOption.value,
+					'--font-color': siteSetting.fontColor.value,
+					'--container-width': siteSetting.contentWidth.value,
+					'--bg-color': siteSetting.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm setSiteSettings={setSiteSettings}/>
 			<Article />
 		</div>
 	);
